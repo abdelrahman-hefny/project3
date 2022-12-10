aacc8bee23958a6747c59eae993dd365be527ca6
abderhman <abdohfny21000@gmail.com>
2022-12-10 22:13:42 +0200

    chaling-3

---
+++ js-ch-3_12-10/test.js
@@ -0,0 +1,11 @@
+let a = 1_00;
+let b = 2_00.5;
+let c = 1e2;
+let d = 2.4;
+
+console.log(Math.max(Math.round(a ,b,c,d,)));
+console.log(a ** Math.round(d));
+console.log(Math.round(d));
+console.log(Math.floor(d));
+console.log(Math.trunc(d));
+console.log(parseInt(d));
